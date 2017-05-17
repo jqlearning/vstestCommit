af631a051901ce3d8a1f8b185edf516441150092
jqsoulmate <916552361@qq.com>
2017-05-17 17:52:34 +0800

    测试

---
+++ pOne1/pOne1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="pOne1.MvcApplication" Language="C#" %>
---
+++ pOne1/pOne1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(pOne1.Startup))]
+namespace pOne1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
